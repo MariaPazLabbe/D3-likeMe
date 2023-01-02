03ad02a967a4067d94deecbe91a4fa842af330c9
María Paz Labbé <104472337+MariaPazLabbe@users.noreply.github.com>
2023-01-01 23:27:10 -0300

    Create all posts

--- posts.js
+++ posts.js
@@ -13,5 +13,13 @@ const getAllPosts = async() => {
   const { rows } = await pool.query("SELECT * FROM posts");
   return rows;
 };
+const createPost = async (payload) => {
+  const consulta =
+    "INSERT INTO posts (titulo, img, descripcion) VALUES ($1, $2, $3)RETURNING * ";
+  const values = [payload.titulo, payload.url, payload.descripcion];
+  const result = await pool.query(consulta, values);
+};
+
+module.exports = { getAllPosts, createPost };
 
 
