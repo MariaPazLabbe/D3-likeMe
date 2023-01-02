6b79bd4241e67cd924c58b6f4b5eece857922767
María Paz Labbé <104472337+MariaPazLabbe@users.noreply.github.com>
2023-01-01 23:25:49 -0300

    connect with postgress

--- posts.js
+++ posts.js
@@ -10,16 +10,4 @@ const pool = new Pool({
     allowExitOnIdle: true,
 });
 
-const getAllPosts = async() => {
-  const { rows } = await pool.query("SELECT * FROM posts");
-  return rows;
-};
 
-const createPost = async (payload) => {
-  const consulta =
-    "INSERT INTO posts (titulo, img, descripcion) VALUES ($1, $2, $3)RETURNING * ";
-  const values = [payload.titulo, payload.url, payload.descripcion];
-  const result = await pool.query(consulta, values);
-};
-
-module.exports = { getAllPosts, createPost };
